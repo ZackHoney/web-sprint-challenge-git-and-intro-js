cdb433b1e908b9aaf796e8d7e88f06e5cee4c19b
Zachary Honey <zhoney466@gmail.com>
2022-10-26 11:30:42 -0700

    Completed 2 more MVP tasks

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ function removeArtist(array, number) {
   return newArr
 }
 
-console.log('Task 5', removeArtist(artists, 5))
+console.log('Task 5', removeArtist(artists, 0))
 
 
 
@@ -296,9 +296,22 @@ Use addArtist to do the following:
 5. Add the newly created object to the copied array, then return the copied array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+function addArtist(array, name, years, genre, nationality, bio) {
+  const newArr = [...array];
+  newArr.push({
+   name: name,
+   years: years,
+   genre: genre,
+   nationality: nationality,
+   bio: bio
+  }
+  )
+   
+   
+    return newArr
+  }
+  
+console.log('Task 6', addArtist(artists, 'Zack Honey', '1993-2022', 'Full Stack Development', 'American',  `I worked in construction for 10 years. I decided it was time to learn something new that wouldn't be so hard on my body physically.`))
 
 
 
@@ -310,10 +323,16 @@ Use lotsOfArt to do the following:
 🌟 EXAMPLE: lotsOfArt(artists) will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]
 */
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(array) {
+  const newArr = [];
+  for (let i = 0; i < array.length; i ++){
+    if(array[i].paintings >= 100){
+      newArr.push(array[i].name)
+    }
+  }return newArr
+  
 }
-
+console.log('Task 7', lotsOfArt(artists))
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 8: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
